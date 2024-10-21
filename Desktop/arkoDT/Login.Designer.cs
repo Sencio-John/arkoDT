33923c63f82ac20a34c20af95b9acbd9364245ed
Sencio-John <senciojohnwyne.bsit@gmail.com>
2024-10-21 18:35:31 +0800

    UI PROBLEM

--- Desktop/arkoDT/Login.Designer.cs
+++ Desktop/arkoDT/Login.Designer.cs
@@ -44,6 +44,7 @@ namespace arkoDT
             // 
             this.pbLogin.Image = ((System.Drawing.Image)(resources.GetObject("pbLogin.Image")));
             this.pbLogin.Location = new System.Drawing.Point(0, 0);
+            this.pbLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbLogin.Name = "pbLogin";
             this.pbLogin.Size = new System.Drawing.Size(556, 733);
             this.pbLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -57,9 +58,10 @@ namespace arkoDT
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblLogin.AutoSize = true;
             this.lblLogin.Font = new System.Drawing.Font("Nirmala UI", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogin.Location = new System.Drawing.Point(1785, 91);
+            this.lblLogin.Location = new System.Drawing.Point(1190, 59);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(248, 96);
+            this.lblLogin.Size = new System.Drawing.Size(167, 65);
             this.lblLogin.TabIndex = 1;
             this.lblLogin.Text = "LOGIN";
             // 
@@ -69,9 +71,10 @@ namespace arkoDT
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(1162, 385);
+            this.txtUsername.Location = new System.Drawing.Point(775, 250);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(1453, 39);
+            this.txtUsername.Size = new System.Drawing.Size(1591, 29);
             this.txtUsername.TabIndex = 3;
             // 
             // txtPassword
@@ -80,10 +83,11 @@ namespace arkoDT
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(1162, 495);
+            this.txtPassword.Location = new System.Drawing.Point(775, 322);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '●';
-            this.txtPassword.Size = new System.Drawing.Size(1453, 39);
+            this.txtPassword.Size = new System.Drawing.Size(1591, 29);
             this.txtPassword.TabIndex = 4;
             // 
             // lblForgotPassword
@@ -94,9 +98,10 @@ namespace arkoDT
             this.lblForgotPassword.AutoSize = true;
             this.lblForgotPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblForgotPassword.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.lblForgotPassword.Location = new System.Drawing.Point(1158, 548);
+            this.lblForgotPassword.Location = new System.Drawing.Point(772, 356);
+            this.lblForgotPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblForgotPassword.Name = "lblForgotPassword";
-            this.lblForgotPassword.Size = new System.Drawing.Size(170, 25);
+            this.lblForgotPassword.Size = new System.Drawing.Size(122, 17);
             this.lblForgotPassword.TabIndex = 6;
             this.lblForgotPassword.Text = "Forgot Password?";
             this.lblForgotPassword.Click += new System.EventHandler(this.lblForgotPassword_Click);
@@ -105,9 +110,10 @@ namespace arkoDT
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label1.Location = new System.Drawing.Point(996, 391);
+            this.label1.Location = new System.Drawing.Point(664, 254);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 32);
+            this.label1.Size = new System.Drawing.Size(102, 24);
             this.label1.TabIndex = 8;
             this.label1.Text = "Username:";
             // 
@@ -115,18 +121,19 @@ namespace arkoDT
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label2.Location = new System.Drawing.Point(1002, 502);
+            this.label2.Location = new System.Drawing.Point(668, 326);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 32);
+            this.label2.Size = new System.Drawing.Size(97, 24);
             this.label2.TabIndex = 9;
             this.label2.Text = "Password:";
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblForgotPassword);
@@ -134,6 +141,7 @@ namespace arkoDT
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.lblLogin);
             this.Controls.Add(this.pbLogin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmLogin";
             this.Text = "Login";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
