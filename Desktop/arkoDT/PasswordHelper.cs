a0c21b9cc3ae5f0b7536e101d0159c3999023a12
Sencio-John <senciojohnwyne.bsit@gmail.com>
2024-10-31 06:29:27 +0800

    PUSH LNAG

--- Desktop/arkoDT/PasswordHelper.cs
+++ Desktop/arkoDT/PasswordHelper.cs
@@ -1,32 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Security.Cryptography;
-using BCrypt.Net;
+using System.Text;
 
 namespace arkoDT
 {
     class PasswordHelper
     {
-        public static string HashPassword(string password)
+        // Constant 16-byte key and IV for AES-128
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("MySecureKey12345");
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("MySecureIV1234567");   
+
+        // Method to encrypt a password
+        public static string EncryptPassword(string password)
         {
-            using (var sha256 = SHA256.Create())
+            using (Aes aes = Aes.Create())
             {
-                var bytes = Encoding.UTF8.GetBytes(password);
-                var hash = sha256.ComputeHash(bytes);
-                return Convert.ToBase64String(hash);
+                aes.Key = Key;
+                aes.GenerateIV(); // Generate a new IV for each encryption
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        // Prepend the IV to the encrypted data
+                        ms.Write(aes.IV, 0, aes.IV.Length); // Write IV to the stream
+                        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var sw = new StreamWriter(cs))
+                            {
+                                sw.Write(password);
+                            }
+                        }
+                        return Convert.ToBase64String(ms.ToArray()); // Return the full encrypted data (IV + ciphertext)
+                    }
+                }
             }
         }
 
-        // Method to verify the password
-        public static bool VerifyPassword(string inputPassword, string hashedPassword)
+        // Method to decrypt a password
+        public static string DecryptPassword(string encryptedPassword)
         {
-            // Hash the input password
-            string hashedInputPassword = HashPassword(inputPassword);
-            // Compare the hashed input password with the stored hashed password
-            return hashedInputPassword == hashedPassword;
+            byte[] fullCipher = Convert.FromBase64String(encryptedPassword);
+
+            using (Aes aes = Aes.Create())
+            {
+                // Extract the IV from the beginning of the cipher
+                byte[] iv = new byte[16]; // 16 bytes for IV
+                Array.Copy(fullCipher, 0, iv, 0, iv.Length); // Get the IV
+
+                // Get the actual encrypted data (excluding IV)
+                byte[] cipher = new byte[fullCipher.Length - iv.Length];
+                Array.Copy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+                aes.Key = Key; // Use the same key for decryption
+                aes.IV = iv;   // Set the extracted IV
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    using (var ms = new MemoryStream(cipher))
+                    {
+                        using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var sr = new StreamReader(cs))
+                            {
+                                return sr.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
