10dc8be34f51dca94d5e28e1400c06e9d3b60bff
xAbakush <evangelista.jc.bsit@gmail.com>
2024-12-17 19:42:09 +0800

    basti's request

--- ARKODesktop/ARKODesktop/Views/Devices.Designer.cs
+++ ARKODesktop/ARKODesktop/Views/Devices.Designer.cs
@@ -364,7 +364,7 @@ namespace ARKODesktop.Views
             // 
             // lblDeviceHeaderTitle
             // 
-            this.lblDeviceHeaderTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(194)))), ((int)(((byte)(236)))));
+            this.lblDeviceHeaderTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
             this.lblDeviceHeaderTitle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblDeviceHeaderTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDeviceHeaderTitle.Location = new System.Drawing.Point(0, 0);
